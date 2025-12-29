d24ebf526edbdebe0bc3f0e9af7b3b418c9ca256
sevketbuyukdemir <sevket.buyukdemir@gmail.com>
2025-12-29 23:07:17 +0300

    MICROSERVICES-6 - product-service project creation.

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 # Config server database
 CREATE DATABASE IF NOT EXISTS configserver;
 USE configserver;
-CREATE USER 'config_server'@'%' IDENTIFIED BY 'config_server';
+CREATE USER IF NOT EXISTS 'config_server'@'%' IDENTIFIED BY 'config_server';
 GRANT ALL PRIVILEGES ON configserver.* TO 'config_server'@'%';
 FLUSH PRIVILEGES;
 CREATE TABLE IF NOT EXISTS PROPERTIES (
@@ -12,3 +12,53 @@ CREATE TABLE IF NOT EXISTS PROPERTIES (
     propertyVALUE TEXT,
     PRIMARY KEY (APPLICATION, PROFILE, LABEL, propertyKEY)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+# product-service database
+CREATE DATABASE IF NOT EXISTS productservice;
+USE productservice;
+CREATE USER IF NOT EXISTS 'product_service'@'%' IDENTIFIED BY 'product_service';
+GRANT ALL PRIVILEGES ON productservice.* TO 'product_service'@'%';
+FLUSH PRIVILEGES;
+CREATE TABLE IF NOT EXISTS products (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `name` varchar(255) NOT NULL,
+    `description`   TEXT,
+    `price`   DECIMAL(10,2) NOT NULL,
+    `currency`  CHAR(3) NOT NULL DEFAULT 'TRY',
+    `active`    BOOLEAN NOT NULL DEFAULT TRUE,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `unique_product` (`name`),
+    INDEX idx_products_active (active)
+) ENGINE=InnoDB ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE IF NOT EXISTS product_categories (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `unique_category` (`name`)
+) ENGINE=InnoDB ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE IF NOT EXISTS product_category_map (
+    `product_id` bigint NOT NULL,
+    `category_id` bigint NOT NULL,
+    PRIMARY KEY (`product_id`,`category_id`),
+    CONSTRAINT `product_id_category_foreign_key` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `category_id_foreign_key` FOREIGN KEY (`category_id`) REFERENCES `product_categories` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE IF NOT EXISTS product_attributes (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `attrType` VARCHAR(255) NOT NULL,
+    `attrValue` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE IF NOT EXISTS product_attribute_map (
+    `product_id` bigint NOT NULL,
+    `attribute_id` bigint NOT NULL,
+    PRIMARY KEY (`product_id`,`attribute_id`),
+    CONSTRAINT `product_id_attribute_foreign_key` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `attribute_id_foreign_key` FOREIGN KEY (`attribute_id`) REFERENCES `product_attributes` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB ROW_FORMAT=DYNAMIC;
